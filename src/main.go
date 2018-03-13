a9e71cfeca078377b3397f50535aac714ac33fab
Dima Condur <kdumitru@gmail.com>
2018-03-13 13:36:02 -0700

    search - linear and binary

---
+++ src/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"./search"
+	"fmt"
+)
+
+func maxRight(arr []int) []int {
+	// Initialize the result array and set the last element as -1
+	var res []int
+	res = make([]int, len(arr))
+	res[len(arr)-1] = -1
+
+	// Save the last element as max right side
+	max := arr[len(arr)-1]
+
+	for idx := len(arr) - 2; idx >= 0; idx-- {
+		res[idx] = max
+		if max < arr[idx] {
+			max = arr[idx]
+		}
+	}
+	return res
+}
+
+func main() {
+	// var a, b, res uint32
+	// fmt.Scanf("%v %v", &a, &b)
+	// res = solveMeFirst(a, b)
+	// fmt.Println(res)
+
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	res := search.Binary(4, arr)
+	fmt.Println(res)
+}
---
+++ src/search/search.go
@@ -0,0 +1,32 @@
+// Package search is a collection of algorithms related to searching is a collection
+// e.g. Given a sorted array arr[] of n elements, write a function to search a given element x in arr[].
+package search
+
+// Linear - find if item x exists is the array, return the index of the item or -1
+func Linear(x int, arr []int) int {
+	for idx, item := range arr {
+		if x == item {
+			return idx
+		}
+	}
+	return -1
+}
+
+// Binary - find if item x exists is the array, return if the item exists or not
+func Binary(x int, arr []int) bool {
+	idxStart := 0
+	idxEnd := len(arr) - 1
+
+	for idxStart <= idxEnd {
+		median := (idxStart + idxEnd) / 2
+		switch {
+		case arr[median] == x:
+			return true
+		case arr[median] < x:
+			idxStart = median + 1
+		default:
+			idxEnd = median - 1
+		}
+	}
+	return false
+}
