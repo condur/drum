aebad24849ff60b5732c22347d4e7ed21f41d421
Dima Condur <kdumitru@gmail.com>
2018-07-28 16:11:01 -0600

    singly linked list finished

--- abstract_data_types/linked_list/singly.py
+++ abstract_data_types/linked_list/singly.py
@@ -6,7 +6,7 @@ class SinglyLinkedList(object):
         self.head = None
         self.size = 0
         for item in iterable:
-            self.append(item)
+            self.append_left(item)
 
     def __iter__(self):
         current = self.head
@@ -14,19 +14,79 @@ class SinglyLinkedList(object):
             yield current.data
             current = current.next
 
-    def append(self, data):
+    def __len__(self):
+        "returns the number of list items"
+        return self.size
+
+    def __str__(self):
+        return str(list(self))
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list.
+        If the index is invalid, return -1.
+
+        :type index: int
+        :rtype: int
+        """
+        for idx, val in enumerate(self):
+            if index == idx:
+                return val
+
+        return -1
+
+    def append(self, val):
+        """inserts a new node at the end of the list"""
+        newNode = Node()
+        newNode.set_data(val)
+
+        if self.head is None:
+            self.head = newNode
+        else:
+            current = self.head
+            while current.get_next() is not None:
+                current = current.get_next()
+            current.set_next(newNode)
+
+        self.size += 1
+
+    def append_left(self, val):
         """inserts a new node at the beginning of the list"""
         newNode = Node()
-        newNode.set_data(data)
+        newNode.set_data(val)
 
         if self.head is None:
             self.head = newNode
         else:
             newNode.set_next(self.head)
             self.head = newNode
+
         self.size += 1
 
-    def delete_from_beginning(self):
+    def append_index(self, index, val):
+        """inserts a new node at the index of the list"""
+        if index > len(self):
+            raise IndexError("Index out of bound exception")
+        elif index == 0:
+            self.append_left(val)
+        elif index == len(self):
+            self.append(val)
+        else:
+            prev = self.head
+            while index > 1 and prev is not None:
+                prev = prev.get_next()
+                index -= 1
+
+            newNode = Node()
+            newNode.set_data(val)
+
+            current = prev.get_next()
+            newNode.set_next(current)
+            prev.set_next(newNode)
+
+            self.size += 1
+
+    def delete_first(self):
         """delete the first node of the linked list"""
         if self.head is None:
             raise IndexError("The list is empty")
@@ -34,19 +94,29 @@ class SinglyLinkedList(object):
             self.head = self.head.get_next()
             self.size -= 1
 
+    def delete_index(self, index):
+        """delete the index node of the linked list"""
+        if index >= len(self):
+            raise IndexError("Index out of bound exception")
+        elif index == 0:
+            self.delete_first()
+        else:
+            prev = self.head
+            while index > 1 and prev is not None:
+                prev = prev.get_next()
+                index -= 1
+
+            prev.set_next(prev.get_next().get_next())
+            self.size -= 1
+
     def clear(self):
         self.head = None
-
-    def get_size(self):
-        "returns the number of list items"
-        return self.size
 
     def calculate_size(self):
         "calculate and returns the number of list items"
 
         count = 0
         current = self.head
-
         while current is not None:
             # increase counter by one
             count += 1
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,9 +1,10 @@
 from abstract_data_types.linked_list.singly import SinglyLinkedList
+import pytest
 
 
-def test_linked_list():
+def test_init_and_len():
     list = SinglyLinkedList([1, 2, 3, 4])
-    assert list.get_size() == 4
+    assert len(list) == 4
     assert list.calculate_size() == 4
 
 
@@ -14,3 +15,38 @@ def test_iter():
     coll.extend(list)
     coll.reverse()
     assert coll == data
+
+
+def test_get():
+    list = SinglyLinkedList([1, 2, 3, 4])
+    assert list.get(0) == 4
+    assert list.get(1) == 3
+    assert list.get(10) == -1
+
+
+def test_append_index():
+    list = SinglyLinkedList([1, 2, 3, 4])
+
+    with pytest.raises(IndexError):
+        list.append_index(5, 5)
+
+    list.append_index(0, 0)
+    assert list.get(0) == 0
+    list.append_index(1, 10)
+    assert list.get(1) == 10
+    list.append_index(6, 6)
+    assert list.get(6) == 6
+
+
+def test_delete_index():
+    list = SinglyLinkedList([1, 2, 3, 4])
+
+    with pytest.raises(IndexError):
+        list.delete_index(5)
+
+    list.delete_index(3)
+    assert len(list) == 3
+    list.delete_index(1)
+    assert len(list) == 2
+    list.delete_index(0)
+    assert len(list) == 1
