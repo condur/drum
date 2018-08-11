0fbd859438d71988ce1eca295615022f6b953c64
Dima Condur <kdumitru@gmail.com>
2018-08-11 12:40:18 -0600

    hash samples, more

--- algo/hash.py
+++ algo/hash.py
@@ -1,3 +1,8 @@
+import collections
+import itertools
+from algo.arrays_strings import partition
+
+
 def containsDuplicate(nums):
     """
     :type nums: List[int]
@@ -43,6 +48,31 @@ def intersection(nums1, nums2):
     return result
 
 
+def intersect(nums1, nums2):
+    """
+    Given two arrays, write a function to compute their intersection.
+
+    Note:
+        1. Each element in the result should appear as many times as
+            it shows in both arrays.
+        2. The result can be in any order.
+
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+    dic1 = collections.defaultdict(list)  # defaults to list
+    for num in nums1:
+        dic1[num].append(num)
+
+    dic2 = collections.defaultdict(list)  # defaults to list
+    for num in nums2:
+        if num in dic1:
+            dic2[num] += dic1[num]
+
+    return list(itertools.chain(*dic2.values()))
+
+
 def isHappy(n):
     """
     Write an algorithm to determine if a number is "happy".
@@ -81,14 +111,9 @@ def twoSum(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    dic = {}
+    dic = collections.defaultdict(list)  # defaults to list
     for idx, num in enumerate(nums):
-        if num in dic:
-            li = dic[num]
-            li.append(idx)
-            dic[num] = li
-        else:
-            dic[num] = [idx]
+        dic[num].append(idx)
 
     for num in nums:
         left = target - num
@@ -96,8 +121,6 @@ def twoSum(nums, target):
             return dic[num]
         elif left != num and left in dic:
             return dic[num] + dic[left]
-
-    return []
 
 
 def twoSum_2(nums, target):
@@ -115,3 +138,115 @@ def twoSum_2(nums, target):
         if complement in dic.keys():
             return [dic[complement], idx]
         dic[key] = idx
+
+
+def isIsomorphic(s, t):
+    """
+    Given two strings s and t, determine if they are isomorphic.
+
+    Two strings are isomorphic if the characters in s can be replaced
+    to get t.
+
+    All occurrences of a character must be replaced with another
+    character while preserving the order of characters.
+    No two characters may map to the same character but a character
+    may map to itself.
+
+    :type s: str
+    :type t: str
+    :rtype: bool
+    """
+    if len(s) != len(t):
+        return False
+
+    dic1, dic2 = {}, {}
+    for item1, item2 in zip(s, t):
+        if item1 in dic1:
+            if dic1[item1] != item2:
+                return False
+        else:
+            dic1[item1] = item2
+
+        if item2 in dic2:
+            if dic2[item2] != item1:
+                return False
+        else:
+            dic2[item2] = item1
+
+    return True
+
+
+def findRestaurant(list1, list2):
+    """
+     Suppose Andy and Doris want to choose a restaurant for dinner,
+     and they both have a list of favorite restaurants represented
+     by strings.
+
+    You need to help them find out their common interest with the
+    least list index sum. If there is a choice tie between answers,
+    output all of them with no order requirement.
+
+    You could assume there always exists an answer.
+
+    :type list1: List[str]
+    :type list2: List[str]
+    :rtype: List[str]
+    """
+    dic1, dic2 = {}, {}
+
+    for idx, item in enumerate(list1):
+        dic1[item] = idx
+
+    for idx, item in enumerate(list2):
+        if item in dic1:
+            dic2[item] = idx + dic1[item]
+
+    minValue = min(dic2.values())
+    return [key for key in dic2 if dic2[key] == minValue]
+
+
+def firstUniqChar(s):
+    """
+    Given a string, find the first non-repeating character in it and
+    return it's index. If it doesn't exist, return -1.
+
+    :type s: str
+    :rtype: int
+    """
+    if len(s) == 0:
+        return -1
+
+    dic = collections.defaultdict(list)
+    for idx, char in enumerate(s):
+        dic[char].append(idx)
+
+    # v1
+    # res = list(filter(lambda x: len(dic[x]) == 1, dic.keys()))
+    # if len(res) > 0:
+    #     min_key = min(res, key=lambda x: dic[x][0])
+    #     return dic[min_key][0]
+
+    # v2
+    for idx, char in enumerate(s):
+        if len(dic[char]) == 1:
+            return idx
+
+    return -1
+
+
+def containsNearbyDuplicate(nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: bool
+    """
+    dic = collections.defaultdict(list)
+    for idx, char in enumerate(nums):
+        dic[char].append(idx)
+
+    for val in filter(lambda x: len(x) > 1, dic.values()):
+        for idxs in filter(lambda x: len(x) > 1, partition(val, 2, 1)):
+            if abs(idxs[0] - idxs[1]) > k:
+                return False
+
+    return True
--- tests/test_hash.py
+++ tests/test_hash.py
@@ -13,6 +13,10 @@ def test_intersection():
     assert hash.intersection([4, 9, 5], [9, 4, 9, 8, 4]) == [4, 9]
 
 
+def test_intersect():
+    assert hash.intersect([4, 9, 5], [9, 4, 9, 8, 4]) == [9, 9, 4, 4]
+
+
 def test_isHappy():
     assert hash.isHappy(19) is True
 
@@ -25,3 +29,32 @@ def test_twoSum():
     assert hash.twoSum_2([2, 7, 11, 15], 9) == [0, 1]
     assert hash.twoSum_2([3, 2, 4], 6) == [1, 2]
     assert hash.twoSum_2([3, 3], 6) == [0, 1]
+
+
+def test_isIsomorphic():
+    assert hash.isIsomorphic("egg", "add") is True
+    assert hash.isIsomorphic("foo", "bar") is False
+    assert hash.isIsomorphic("ab", "aa") is False
+    assert hash.isIsomorphic("ab", "ca") is True
+
+
+def test_findRestaurant():
+    assert hash.findRestaurant(
+        ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+        ["Piatti", "The Grill at Torrey Pines", "Hungry Hunter Steakhouse", "Shogun"],
+    ) == ["Shogun"]
+
+    assert hash.findRestaurant(
+        ["Shogun", "Tapioca Express", "Burger King", "KFC"],
+        ["KFC", "Shogun", "Burger King"],
+    ) == ["Shogun"]
+
+
+def test_firstUniqChar():
+    assert hash.firstUniqChar("loveleetcode") == 2
+
+
+def test_containsNearbyDuplicate():
+    assert hash.containsNearbyDuplicate([1, 2, 3, 1], 3) is True
+    assert hash.containsNearbyDuplicate([1, 0, 1, 1], 1) is True
+    assert hash.containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2) is False
